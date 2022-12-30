368d1c3058c5baa91e1546d8e4149315a9a371a5
Konstantin <iscoulfi@gmail.com>
2022-12-30 18:12:04 +0500

    HasRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { fetchPizzas } from './redux/slices/pizzaSlice';
 import { useAppDispatch } from './redux/store';
@@ -35,7 +35,7 @@ function App() {
   }, [categoryId, sort, value, currentPage]);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <div className="wrapper">
           <Header />
@@ -48,7 +48,7 @@ function App() {
           </div>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
